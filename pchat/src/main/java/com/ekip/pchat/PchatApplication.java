739348f21672702fb09c16f302ea24825b8465da
sefacekerek <sefacekerek2014.pa@gmail.com>
2023-03-11 15:39:33 +0100

    initiliaze project

---
+++ pchat/src/main/java/com/ekip/pchat/PchatApplication.java
@@ -0,0 +1,13 @@
+package com.ekip.pchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PchatApplication.class, args);
+    }
+
+}
